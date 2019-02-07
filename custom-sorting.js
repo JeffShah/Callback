244696a9a7c676e756257fff6847c97e74bda47e
JaffarShah <sjeffshah@gmail.com>
2019-02-07 21:20:22 +0000

    Custom Sorting assignment

--- custom-sorting.js
+++ custom-sorting.js
@@ -1,6 +1,21 @@
 var students = [
   { id: 1, name: "bruce",    age: 40 },
   { id: 2, name: "zoidberg", age: 22 },
-  { id: 3, name: "alex",     age: 22 },
-  { id: 4, name: "alex",     age: 30 }
+  { id: 3, name: "alex",     age: 45 },
+  { id: 4, name: "alex",     age: 35}
 ];
+
+students.sort(function(a, b) {
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name === a.name) {
+    return a.age - b.age;
+  }
+  return 0;
+});
+
+console.log(students);
