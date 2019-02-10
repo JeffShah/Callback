5054119bc4bd6f1f0ef5248f46aed53819203251
JaffarShah <sjeffshah@gmail.com>
2019-02-10 01:47:08 +0000

    Callback Map assignment

---
+++ callback-map.js
@@ -0,0 +1,24 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+var wordLength = words.map(function(num) {
+  var output = [];
+  var wordLength =
+  console.log(num.length);
+  return word.length;
+});
+
+var wordToUpperCase = word.map(function(num) {
+  var output = [];
+  return word.toUpperCase();
+});
+
+var wordReversed = word.map(function(num) {
+  return word.split('').reverse().join('');
+});
+
+console.log(wordLength);
+console.log(wordToUpperCase);
+console.log(wordReversed);
+
+
+
