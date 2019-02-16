ac1656dfc75acb005b2751feb52d7303a1f7cf12
JaffarShah <sjeffshah@gmail.com>
2019-02-16 23:35:47 +0000

    Modified call-map.js

--- callback-map.js
+++ callback-map.js
@@ -20,5 +20,39 @@ console.log(wordLength);
 console.log(wordToUpperCase);
 console.log(wordReversed);
 
+/*
 
+var words = ["ground", "control", "to", "major", "tom"];
+
+function map(arr, func) {
+  var output = [];
+
+  arr.forEach(function(elem){
+    output.push(func(elem));
+  })
+
+  return output;
+};
+
+var length = function(word) {
+  return word.length;
+};
+
+console.log(map(words, length))
+
+var uppCase = function(word) {
+  return word.toUpperCase();
+};
+
+console.log(map(words, uppCase));
+
+var reversed = function(word) {
+  return word.split('').reverse().join('');
+};
+
+console.log(map(words, reversed));
+
+
+
+*/
 
