de7c5876b58e767ef0fa9d8244eddf410c75e9b2
JaffarShah <sjeffshah@gmail.com>
2019-04-04 13:44:51 -0400

    Comments and revised

--- custom-sorting.js
+++ custom-sorting.js
@@ -5,7 +5,7 @@ var students = [
   { id: 4, name: "alex",     age: 35}
 ];
 
-
+// Using sort function inside the Javascript which is pre-defined by the JS language.
 students.sort(function(a, b) {
   if (a.name > b.name) {
     return 1;
@@ -20,3 +20,16 @@ students.sort(function(a, b) {
 });
 
 console.log(students);
+
+// How we know what is a, and b?
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+// Explaining the function called, student.sort:
+/* a: here means that the script will run back into var student which is an array [],
+ and will read the first set of the information. 
+ By using dot notation "." with a.name, it will connect/join the next set of information.
+
+ I used if-statement here, which one has to, is because we have different conditins.
+
+*/
